9f522707b2bc07df80b387859b98bc1b5b808dbf
Tiffany Poget <tiffany.poget@gmail.com>
2026-04-08 17:50:06 +0200

    Fix: simpler Vite JSX config for .js files

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: '**/*.{jsx,js}',
+    }),
+  ],
   build: {
     outDir: 'build',
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js$/,
   },
   optimizeDeps: {
     esbuildOptions: {
