49c66670c4fb42c4e80cb8a1da4f8b068bb370e1
tiffanypoget-ai <tiffany.poget@gmail.com>
2026-04-16 19:50:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
-    react({
-      include: /\.(jsx?|tsx?)$/,
-    }),
+    react(),
   ],
   build: {
     outDir: 'build',
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
   esbuild: {
     loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
+    include: /.*\.js$/,
   },
 });
